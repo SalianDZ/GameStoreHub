4ac6aa845e8adbee372760a978b1b605c310acbc
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2024-03-05 17:15:34 +0200

    Added extension folder with claimprincipalextension

---
+++ GameStoreHub.Web/GameStoreHub.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace GameStoreHub.Web.Infrastructure.Extensions
+{
+	public static class ClaimsPrincipalExtensions
+	{
+		public static string GetId(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+	}
+}
