08dd54b62ca71e3a9a8d9c6bb3c02ee2efd4b668
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2024-03-11 20:24:37 +0200

    Implemented custom registration

--- GameStoreHub.Web/GameStoreHub.Web/Controllers/UserController.cs
+++ GameStoreHub.Web/GameStoreHub.Web/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using GameStoreHub.Data.Models;
+using GameStoreHub.Web.ViewModels.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameStoreHub.Web.Controllers
 {
-    public class UserController : Controller
+	public class UserController : Controller
     {
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly UserManager<ApplicationUser> userManager;
@@ -18,9 +19,41 @@ namespace GameStoreHub.Web.Controllers
             this.userStore = userStore;
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = new()
+            { 
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            await userManager.SetUserNameAsync(user ,model.Email);
+            await userManager.SetEmailAsync(user ,model.Email);
+            IdentityResult result = await userManager.CreateAsync(user);
+
+            if (!result.Succeeded) 
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                } 
+
+                return View(result);
+            }
+			await signInManager.SignInAsync(user, false);
+			return RedirectToAction("Index", "Home");
+		}
     }
 }
